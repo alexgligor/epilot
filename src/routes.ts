324d7720743106b745eef6c65b3091eb154ff09b
Alexandru Gligor <Alexandru.Gligor@honeywell.com>
2021-01-28 14:36:28 +0200

    Adding project files

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { activeUsersController } from './controllers/active-users';
+import { downwardsController } from './controllers/downwards-repos';
+
+export const routes = express.Router();
+
+routes.get('/v1/active/:userName', activeUsersController);
+routes.get('/v1/downwards/:repoName', downwardsController);
